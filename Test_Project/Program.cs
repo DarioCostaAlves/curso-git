76c1a0386645784202e700e485d8c75225ce6ceb
Dario Costa <dario.c.alves22@gmail.com>
2020-04-21 14:19:30 +0100

    Projeto inicial de teste para o git

---
+++ Test_Project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
